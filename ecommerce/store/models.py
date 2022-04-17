ca9bf0702e2db38798aa0c62d6139e24cf670e7a
codeshubham <shubhammank@gmail.com>
2022-04-17 17:12:02 +0530

    models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model): 
+    name = models.CharField(max_length=255,db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creater')
+    title = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank = True)
+    image = models.ImageField(upload_to = 'images/')
